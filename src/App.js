0429bf25c18ec63d7ed23aa4deebdff4f88bcba1
NMRayhan <nurmdrayhan97@gmail.com>
2022-04-05 05:05:30 +0600

    Header Component Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Blog from "./component/Blog/Blog";
+import Contact from "./component/Contact/Contact";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
+import Page404 from "./component/Page404/Pag404";
+import Review from "./component/Review/Review";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="*" element={<Page404></Page404>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <ul className='nav-bar'>
+                <li><CustomLink to="/">Home</CustomLink></li>
+                <li><CustomLink to="/review">Review</CustomLink></li>
+                <li><CustomLink to="/blog">Blog</CustomLink></li>
+                <li><CustomLink to="/contact">Contact</CustomLink></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
